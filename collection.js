6b8f441483188711bf37530caa14a7320885893d
Your Name <mengat.khalid01@gmail.com>
2020-11-13 10:53:16 +0100

    js update

---
+++ collection.js
@@ -0,0 +1,159 @@
+const collection = [
+    {
+      title: 'Naruto',
+      author: 'Masashi Kishimoto',
+      category: ['Aventure'],
+      img: 'pics/uzumaki.jpg',
+      link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+      description:  "L'histoire commence pendant l'adolescence de Naruto, vers ses douze ans. Orphelin, éternel cancre et grand farceur, il fait toutes les bêtises possibles pour se faire remarquer. Son rêve : devenir Hokage afin d'être reconnu par les habitants de son village. En effet, le démon scellé en lui a attisé la crainte et le mépris des autres villageois, qui, avec le temps, ne font plus de différence entre le Kyūbi et Naruto."
+    },
+
+    {
+        title: 'One Piece',
+        author: 'Eiichiro Oda',
+        category: ['Aventure'],
+        img: 'pics/op.png',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Gloire, fortune et puissance, c'est ce que possédait Gold Roger, le tout puissant roi des pirates, avant de mourir sur l'échafaud. Mais ses dernières paroles ont éveillées bien des convoitises, et lança la fabuleuse 'ère de la piraterie', chacun voulant trouver le fabuleux trésor qu'il disait avoir laissé."
+      },
+
+      {
+        title: 'Dragon Ball',
+        author: 'Akira Toriyama',
+        category: ['Action'],
+        img: 'pics/dbzz.png',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Alors qu'elle parcourt les routes de montagnes à moto, Bulma fait une bien étrange rencontre en la personne de Sangoku, un petit garçon étonnamment fort, résistant et possédant une queue, comme les singes. Il possède un trésor qu'elle recherche, une boule de cristal, mais ne veut pas la lui céder, c'est un cadeaux que lui a laissé son grand-père Sangohan. Ils trouvent finalement un compromis, et Sangoku part avec elle à la recherche des 7 boules de cristal dont on dit qu'elles exaucent n'importe quel souhait une fois réunis."
+      },
+
+      {
+        title: 'Bleach',
+        author: 'Tite Kubo',
+        category: ['Surnaturel'],
+        img: 'pics/kuro.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Kurosaki Ichigo, un étudiant de quinze ans aux cheveux orange qui aime la bagarre (comme son père) a la particularité de voir les fantômes ainsi que de pouvoir les toucher. Cela l'amène à rencontrer Kuchiki Rukia, un Shinigami (dieu de la mort) qui combat un Hollow. Le déroulement du combat amène Kuchiki à donner ses pouvoirs à Ichigo qui deviens alors lui même un Shinigami. C'est maintenant à son tour de protéger la ville des Hollows."
+      },
+
+      {
+        title: 'Hunter X Hunter',
+        author: 'Yoshihiro Togashi',
+        category: ['Action'],
+        img: 'pics/hxhh.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Gon Freeks est un jeune garçon provenant d'un petit village aimé des animaux. Il rêve de devenir Hunter afin de retrouver son père, le plus grand Hunter de tous, qu'il n'a jamais vu qu'en photo. Mais pour devenir Hunter professionnel, il faut passer plusieurs épreuves et tests très difficiles. Pendant les épreuves, Gon fait la connaissance d'amis tel que Kirua, Kurapica, Leorio mais de très dangereux combattants sont également présent afin de passer le test de Hunter."
+      },
+
+      {
+        title: 'Black Clover',
+        author: 'Yuuki Tabata',
+        category: ['Fantasie'],
+        img: 'pics/clover.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Asta et Yuno sont deux adolescents de 15 ans qui ont grandi ensemble dans un petit village du royaume de Clover. Leur but ultime à tous les deux est de devenir le prochain empereur-mage ! Étrangement, dans ce royaume, Asta est le seul qui soit incapable de produire la moindre étincelle magique. Loin d'être découragé, il participe tout de même avec son ami (et rival) Yuno à la cérémonie annuelle de remise des grimoires qui rassemble les jeunes de 15 ans dans tout le royaume de Clover. Lors de cette cérémonie, Yuno recevra un grimoire légendaire. Ce qui n'est pas le cas d'Asta car malheureusement pour lui, aucun grimoire ne lui sera attribué ce jour là. Mais Asta ne se laissera pas abattre, peut importe les efforts qu'il aura à fournir, il compte bien rivaliser avec Yuno pour aller au bout de son rêve."
+      },
+
+      {
+        title: 'Kuroko No Basket',
+        author: 'Tadatoshi Fujimaki',
+        category: ['Sport'],
+        img: 'pics/baskett.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "La Génération Miracle, la fameuse équipe du collège Teikou composée de cinq génies. Une légende raconte qu'un sixième membre fantôme aurait fait partie de cette équipe, un membre que les cinq autres respectaient et considéraient comme leur supérieur. C'est la rentrée des classes au lycée Seirin et le club de basket compte désormais dans ses rangs Taiga Kagami, joueur impulsif et prometteur revenu d'Amérique, qui rêve d'écraser la Génération Miracle et devenir le numéro 1 du Japon, et Kuroko Tetsuya, garçon très effacé que personne ne remarque et peu doué pour le basket. Ce dernier aurait pourtant fait partie de la fameuse équipe de Teikou, à la grande surprise de tout le monde. Très vite, Kuroko s'intéressera à Kagami et décidera de l'aider à devenir le meilleur du Japon."
+      },
+
+      {
+        title: 'Haikyuu',
+        author: 'haruichi Furudate',
+        category: ['Sport'],
+        img: 'pics/haikyuuu.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Shôyô Hinata, surnommé Shô, aime plus que tout jouer au volley-ball et ce, malgré sa petite taille. Malheureusement, suite à une sévère défaite, son club de collège a été dissous, tous les membres étant partis. Mais Shô est bien décidé à jouer de nouveau et choisit son futur lycée en fonction de son ambition. Il intègre donc le lycée Karasuno, où a joué son idole, le Petit Géant, tout en espérant faire aussi bien que lui."
+      },
+
+      {
+        title: 'My Hero Academia',
+        author: 'Kohei Horikoshi',
+        category: ['Action'],
+        img: 'pics/mhaa.png',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Dans un monde où 80 % de la population possède un super‑pouvoir appelé alter, les héros font partie de la vie quotidienne. Et les super‑vilains aussi ! Face à eux se dresse l'invincible All Might, le plus puissant des héros ! Le jeune Izuku Midoriya en est un fan absolu. Il n'a qu'un rêve : entrer à la Hero Academia pour suivre les traces de son idole. Le problème, c'est qu'il fait partie des 20 % qui n'ont aucun pouvoir... Son destin est bouleversé le jour où sa route croise celle d'All Might en personne ! Ce dernier lui offre une chance inespérée de voir son rêve se réaliser. Pour Izuku, le parcours du combattant ne fait que commencer !"
+      },
+
+      {
+        title: 'Shingeki No Kiyojin',
+        author: 'Hajime Isayama',
+        category: ['Epouvante'],
+        img: 'pics/snkk.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Eren est un petit garçon rêvant de voyager dans le monde extérieur. Mais cela est impossible car il vit dans une ville fortifiée aux murailles dépassant les cinquante mètres de haut. Ces remparts sont nécessaires à la sécurité des habitants car ils sont les derniers représentants de l'humanité, obligés de se cacher pour échapper aux titans qui ont massacré la majeure partie du genre humain un siècle plus tôt. Eren est têtu : il ne se doute pas de la violence des combats qui ont opposé les hommes aux titans. Il décide de poster sa candidature pour devenir éclaireur, car il s'agit des seuls soldats autorisés à sortir de la ville pour en savoir plus sur les titans. Mais un beau jour, un géant parvient à détruire le mur qui protégeait la ville et ouvre la voie à plusieurs dizaines de ses congénères ! Eren assiste impuissant à la mort atroce de sa mère qui finit dévorée. Alors qu'il fuit avec d'autres survivants, il se jure de se venger et de détruire la race des titans jusqu'à ce qu'il n'en reste plus un seul !!"
+      },
+
+      {
+        title: 'Demon Slayer',
+        author: 'Koyoharu Gotoge',
+        category: ['Aventure'],
+        img: 'pics/demon.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Depuis les temps anciens, il existe des rumeur concernant des démons mangeurs d'hommes qui se cachent dans les bois. Pour cette raison, les citadins locaux ne s'y aventurent jamais la nuit. La légende raconte aussi qu'un tueur de démons déambule la nuit, chassant ces démons assoiffés de sang. Pour le jeune Tanjirou, ces rumeurs vont bientôt devenir sa dure réalité ... Depuis la mort de son père, Tanjirou a pris sur lui pour subvenir aux besoins de sa famille. Malgré cette tragédie, ils réussissent à trouver un peu de bonheur au quotidien. Cependant, ce peu de bonheur se retrouve détruit le jour où Tanjirou découvre que sa famille s'est faite massacrer et que la seule survivante, sa sœur Nezuko, est devenue un démon. À sa grande surprise, Nezuko montre encore des signes d'émotion et de pensées humaines. Ainsi, commence la dure tâche de Tanjirou, celle de combattre les démons et de faire redevenir sa sœur humaine"
+      },
+
+      {
+        title: 'GTO',
+        author: 'Toru Fujisawa',
+        category: ['Comedie'],
+        img: 'pics/gto.jpg',
+        link: 'https://www.youtube.com/watch?v=DjITaSMdgpU&ab_channel=iampkbeast',
+        description:  "Ancien voyou, chef de gang, Eikichi Onizuka décide un jour de devenir prof. Sa vocation n'a rien de pédagogique. Ce qu'il veut, c'est pouvoir sortir avec les étudiantes du lycée où il travaille. Pourtant, au fil des chapitres, on se rend compte que le sens de la justice et de l'honneur de notre héros vont l'amener à devenir un véritable enseignant. Cette série de Tôru Fujisawa a connu un tel succès au Japon qu'elle fut adaptée en feuilleton ' live ' à la télé et au cinéma. Drôle et ' vrai ', plein de rebondissements et doté d'une psychologie rare dans ce domaine, GTO met en scène des adolescents en quête d'identité comme il en existe partout"
+      },
+  ]
+  document.querySelector('.supp').innerHTML = ''; 
+  const mainContainer = document.querySelector('.supp');
+
+  for (let element of collection ){
+    const colonnes = document.createElement('div');
+      colonnes.className = 'col-12 col-sm-6 col-md-4 col-xl-3 mb-4';
+      mainContainer.appendChild(colonnes);
+
+    const card = document.createElement('div');
+      card.className = 'card bg-secondary text-white';
+      colonnes.appendChild(card);
+
+    const image = document.createElement('img');
+      image.className = 'card-img-top';
+      image.src = element.img;
+      card.appendChild(image);
+
+    const card_body = document.createElement('div');
+      card_body.className = 'card-body';
+      card.appendChild(card_body); 
+      
+    const badge = document.createElement('span');
+      badge.className = 'badge';
+      badge.style = 'background-color : brown; color : white';
+      badge.textContent = element.category;
+      card_body.appendChild(badge);
+
+    const title = document.createElement('h5');
+      title.className = 'card_title';
+      title.textContent = element.title;
+      card_body.appendChild(title);
+
+    const auteur = document.createElement('h6');
+      auteur.className = 'card-subtitle mb-2 text-muted';
+      auteur.textContent = element.author;
+      card_body.appendChild(auteur);
+
+    const description = document.createElement('p');
+      description.className = 'card-text overflow-auto';
+      description.style= 'font-size: small;height:8rem';
+      description.textContent = element.description;
+      card_body.appendChild(description);
+
+    const youtube = document.createElement('a');
+      youtube.className = 'fab fa-youtube';
+      youtube.style = 'color: red';
+      youtube.href = element.link;
+      card_body.appendChild(youtube);
+
+  }
